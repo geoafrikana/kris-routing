ee4f645c7d6c34d0bde894eb0cf334bbd35808c8
geoafrikana <geoafrikana@gmail.com>
2025-05-06 21:36:48 +0100

    create github actions workflow

--- scripts.sh
+++ scripts.sh
@@ -1,2 +1,6 @@
 sudo apt install osm2pgsql
 sudo apt install osmium-tool
+psql \
+"postgresql://$POSTGRES_USER:$PGPASSWORD@localhost:5432/kris" \
+-c "CREATE EXTENSION postgis; CREATE EXTENSION hstore; CREATE EXTENSION pgrouting;"
+
