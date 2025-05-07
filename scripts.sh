1e3b853c27831858d234203525cb128f24a5586e
geoafrikana <geoafrikana@gmail.com>
2025-05-07 19:55:30 +0100

    upload yaounde data to db

--- scripts.sh
+++ scripts.sh
@@ -4,3 +4,7 @@ psql \
 "postgresql://$POSTGRES_USER:$PGPASSWORD@localhost:5432/kris" \
 -c "CREATE EXTENSION postgis; CREATE EXTENSION hstore; CREATE EXTENSION pgrouting;"
 
+PGPASSWORD=$PGPASSWORD osm2pgsql --host localhost -U $POSTGRES_USER \
+	--database $POSTGRES_DB --verbose  \
+	--hstore-all --create yaounde.pbf
+
