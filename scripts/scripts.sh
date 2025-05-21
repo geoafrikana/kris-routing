0c512e11a8a843f1472031af2453f36ff8689d92
geoafrikana <geoafrikana@gmail.com>
2025-05-21 15:42:02 +0100

    Use Yaounde and Douala boundary

--- scripts/scripts.sh
+++ scripts/scripts.sh
@@ -6,15 +6,19 @@ psql \
 "postgresql://$POSTGRES_USER:$PGPASSWORD@localhost:5432/kris" \
 -c "CREATE EXTENSION hstore; CREATE EXTENSION pgrouting CASCADE;"
 
+# YAOUNDE_DOUALA_BOUNDING_BOX: 9.3358,3.7241,11.5766,4.2258 
+osmium extract -b 9.3358,3.7241,11.5766,4.2258 \
+             cameroon-latest.osm.pbf -o yaounde_douala.pbf
+
 PGPASSWORD=$PGPASSWORD osm2pgsql --host localhost -U $POSTGRES_USER \
 	--database $POSTGRES_DB --verbose  \
-	--hstore-all --create yaounde.pbf
+	--hstore-all --create yaounde_douala.pbf
 
-osmconvert yaounde.pbf \
+osmconvert yaounde_douala.pbf \
 	--drop-author --drop-version \
-	--out-osm -o=yaounde_reduc.osm
+	--out-osm -o=yaounde_douala.osm
 
-osm2pgrouting --f yaounde_reduc.osm \
+osm2pgrouting --f yaounde_douala.osm \
 	--dbname $POSTGRES_DB \
 	--username $POSTGRES_USER --clean \
     -W $PGPASSWORD
